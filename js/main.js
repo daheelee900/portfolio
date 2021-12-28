5782d80e0b5c037618cd2430eb8802f7194d94bf
daheelee900 <65429660+daheelee900@users.noreply.github.com>
2021-12-28 14:20:37 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+    $("#skillbar_java").animate({width:'70%'},1500);
+    $("#skillbar_html").animate({width:'85%'},1500);
+    $("#skillbar_spring").animate({width:'75%'},1500);
+    $("#skillbar_android").animate({width:'50%'},1500);
+    $("#skillbar_type").animate({width:'30%'},1500);
+    $("#skillbar_sql").animate({width:'70%'},1500);
+    $("#skillbar_css").animate({width:'70%'},1500);
+    $("#skillbar_javas").animate({width:'70%'},1500);
+    $("#skillbar_jquery").animate({width:'80%'},1500);
+    $("#skillbar_react").animate({width:'60%'},1500);
+    
+    });
